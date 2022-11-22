668071552c473c6fdf3b394c182074627ab30f50
Jayven Nhan <nhantrivinh@gmail.com>
2022-11-21 22:25:19 -0400

    Add collection protocols and view models

--- FootballUI/Sources/FootballUI/CollectionUIHandler.swift
+++ FootballUI/Sources/FootballUI/CollectionUIHandler.swift
@@ -6,12 +6,65 @@
 //
 
 import UIKit
+import Combine
 
-protocol CollectionUIHandler: AnyObject {
-//    associatedtype ListObject: Hashable
-    typealias Snapshot = NSDiffableDataSourceSnapshot<String, String>
-    typealias DataSource = UICollectionViewDiffableDataSource<String, String>
-    typealias CellConfiguration = (UICollectionViewListCell, String) -> Void
-    typealias DequeueCellOperation = (UICollectionView, IndexPath, String) -> UICollectionViewListCell?
+protocol CollectionUILoadable: AnyObject {
+    associatedtype ListObject: Hashable
+    typealias Snapshot = NSDiffableDataSourceSnapshot<String, ListObject>
+    typealias DataSource = UICollectionViewDiffableDataSource<String, ListObject>
+    typealias CellConfiguration = (UICollectionViewListCell, ListObject) -> Void
+    typealias DequeueCellOperation = (UICollectionView, IndexPath, ListObject) -> UICollectionViewListCell?
     var collectionView: UICollectionView { get }
+}
+
+final class CollectionUIHandler<ListObject: Hashable>: CollectionUILoadable {
+    // MARK: - Properties
+    let collectionView: UICollectionView
+    var cellConfiguration: CellConfiguration?
+    var dequeueHeaderCellOperation: DequeueCellOperation?
+    private(set) var hashableObjects = CurrentValueSubject<[ListObject], Never>([])
+    private lazy var customLayout: UICollectionViewLayout = {
+        let layout = UICollectionViewCompositionalLayout { _, layoutEnvironment -> NSCollectionLayoutSection? in
+            var listConfiguration = UICollectionLayoutListConfiguration(
+                appearance: .insetGrouped
+            )
+            let listCollectionLayoutSection = NSCollectionLayoutSection.list(
+                using: listConfiguration, layoutEnvironment: layoutEnvironment
+            )
+            return listCollectionLayoutSection
+        }
+        return layout
+    }()
+    lazy var dataSource: DataSource = {
+        let listCellRegistration = UICollectionView.CellRegistration
+        <UICollectionViewListCell, ListObject> { [weak self] cell, _, object in
+            guard let cellConfiguration = self?.cellConfiguration else { return }
+            cellConfiguration(cell, object)
+        }
+        let dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, object in
+            guard let dequeueCellOperation = self.dequeueHeaderCellOperation else {
+                return collectionView.dequeueConfiguredReusableCell(
+                    using: listCellRegistration,
+                    for: indexPath,
+                    item: object
+                )
+            }
+            return dequeueCellOperation(collectionView, indexPath, object)
+        }
+        return dataSource
+    }()
+    // MARK: - Initializers
+    init(collectionView: UICollectionView) {
+        self.collectionView = collectionView
+    }
+    // MARK: - Data Source
+    func applyDataSourceSnapshot(animatingDifferences: Bool = true) {
+        applyDataSourceSnapshot(hashableObjects: hashableObjects.value, animatingDifferences: animatingDifferences)
+    }
+    func applyDataSourceSnapshot(hashableObjects: [ListObject], animatingDifferences: Bool = true) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([""])
+        snapshot.appendItems(hashableObjects)
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
 }
