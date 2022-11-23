2feee667638d8f1953fd04d3bc2e1c26172344de
Jayven Nhan <nhantrivinh@gmail.com>
2022-11-23 01:50:46 -0400

    Update Package.swift

--- FootballAWS/Package.swift
+++ FootballAWS/Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FootballAWS",
-            dependencies: ["FootballCore"]),
+            dependencies: []),
         .testTarget(
             name: "FootballAWSTests",
             dependencies: ["FootballAWS"]),
